408b7da98d8e3b2c3fc496066046b200a824829b
Rosa Pavón <mrosapavon@gmail.com>
2021-06-21 12:37:17 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {useParams} from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { useState } from 'react';
+import userEvent from '@testing-library/user-event';
+
+function Saludo(){
+  let params = useParams();
+  let nombre = params.nombre;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <p>Hola! {nombre}</p>
+  )
 }
 
+
+function App(){
+  const [input, setInput]=useState("")
+
+  function cambiarInput(e){
+    setInput(e.target.value)
+  }
+ 
+return (
+<BrowserRouter>
+       <Route exact path='/saludo/:nombre'>
+       <Link to={`/`}>inicio</Link>
+        <Saludo />
+        </Route>
+      <Route exact path='/'>
+        <h2>Hola!</h2>
+        <form>
+        <input type="text" onChange={cambiarInput} value={input}></input>
+        <Link to={`/saludo/${input}`}>Saludo</Link>
+        </form>
+   
+        </Route>
+</BrowserRouter>
+
+)
+}
 export default App;
+
